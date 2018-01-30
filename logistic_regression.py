990d1fa69391aa7d9c82f8ddb5b975c291959b52
Nafseer Kadiyaravida <nafseerk@gmail.com>
2018-01-30 00:48:27 -0500

    Added python notebook

--- logistic_regression.py
+++ logistic_regression.py
@@ -1,6 +1,7 @@
 from data_loader import DataLoader
 import numpy as np
 import pandas as pd
+import pprint
 
 
 class LogisticRegression:
@@ -9,17 +10,18 @@ class LogisticRegression:
         self.M = M
         self.y_map = {5.0: 1, 6.0: 0}
         self.w_bar = None
-        self.max_iterations = 50
+        self.max_iterations = 10
         self.train_accuracy = None
 
     def sigmoid(self, x):
         power = -1 * np.add(np.matmul(np.transpose(self.w_bar[1:]), x), self.w_bar[0, 0])
         return 1 / (1 + np.exp(power))
 
-    def get_gradient_and_hessian(self, dataset):
+    def get_gradient_and_hessian_and_log_likelihood(self, dataset):
 
         gradient = np.zeros((self.M + 1, 1), dtype=float)
         H = np.zeros((self.M + 1, self.M + 1), dtype=float)
+        log_likelihood = 0
         for train_set_attrs, train_set_labels in dataset:
 
             if len(train_set_attrs) != len(train_set_labels):
@@ -32,23 +34,31 @@ class LogisticRegression:
                 sigmoid_value = self.sigmoid(xi)
                 gradient = np.add(gradient, (sigmoid_value - yi) * xi_bar)
                 H = np.add(H, sigmoid_value * (1 - sigmoid_value) * np.matmul(xi_bar, np.transpose(xi_bar)))
+                log_likelihood += (yi * np.log(sigmoid_value) + (1 - yi) * np.log(1 - sigmoid_value))
 
-        return H, gradient
+        return H, gradient, log_likelihood
 
-    def learn(self, dataset, verbose=False, report_acc=False):
+    def learn(self, dataset, report_acc=False, max_iterations=10):
 
-        # TODO - set initial values for weights
+        self.max_iterations = max_iterations
+
         self.w_bar = np.zeros((self.M + 1, 1))
+        small_change = 0.0000000001
+        log_likelihood_old = self.get_gradient_and_hessian_and_log_likelihood(dataset)[2]
+        change_in_log_likelihood = np.Infinity
 
-        # TODO - add logic for convergence check
-        for i in range(self.max_iterations):
-            H, gradient = self.get_gradient_and_hessian(dataset)
+        i = 0
+        while abs(change_in_log_likelihood) > small_change and i < self.max_iterations:
+            H, gradient, log_likelihood_new = self.get_gradient_and_hessian_and_log_likelihood(dataset)
             H_inv = np.linalg.inv(H)
             self.w_bar = self.w_bar - np.matmul(H_inv, gradient)
+            change_in_log_likelihood = log_likelihood_old - log_likelihood_new
+            log_likelihood_old = log_likelihood_new
+            i += 1
 
         if report_acc:
-            self.train_accuracy = self.k_fold_cross_validation(full_dataset)
-            print('Training Accuracy = %.2f %%' % self.train_accuracy)
+            self.train_accuracy = self.k_fold_cross_validation(dataset)
+            print('Training Accuracy = %.3f %%' % self.train_accuracy)
 
     def classify_point(self, x):
         prob_y_given_x = self.sigmoid(x)[0][0]
@@ -82,29 +92,31 @@ class LogisticRegression:
         return predicted_labels, accuracy
 
     def k_fold_cross_validation(self, dataset, k=10):
+        cv_test_model = LogisticRegression(M=self.M)
         avg_accuracy = 0.0
         for i in range(k):
             test_attrs, test_labels = dataset.pop(0)
-            accuracy = self.classify(test_attrs, true_labels=test_labels)[1]
+            cv_test_model.learn(dataset)
+            accuracy = cv_test_model.classify(test_attrs, true_labels=test_labels)[1]
             dataset.append((test_attrs, test_labels))
             avg_accuracy += accuracy
-        return avg_accuracy / k
+        self.train_accuracy = avg_accuracy / k
+        print('Training Accuracy', self.train_accuracy)
+        return self.train_accuracy
+
+    def summary(self):
+        print('\n=====Model Summary=====')
+        print('w0 =', self.w_bar[0])
+        print('\nw of size', end=' ')
+        print(self.w_bar[1:].shape, ':')
+        pprint.pprint(self.w_bar[1:])
+        if self.train_accuracy:
+            print('Training Accuracy = %.3f %%' % self.train_accuracy)
 
 
 if __name__ == '__main__':
     full_dataset = DataLoader.load_full_dataset('./dataset')
     log_reg = LogisticRegression(M=64)
-    log_reg.learn(full_dataset, report_acc=True)
+    log_reg.learn(full_dataset)
 
-    # Test the model with test data taken from training data
-    train_dataset, test_attrs, test_labels = DataLoader.load_with_test_data(
-        './dataset',
-        split_ratio=0.1)
-
-    log_reg = LogisticRegression(M=64)
-    log_reg.learn(train_dataset)
-    predictions, acc = log_reg.classify(test_attrs, true_labels=test_labels)
-
-    print('Test Accuracy = %.2f %%' % acc)
-    print('=====Predictions=====')
-    print(predictions)
+    log_reg.summary()
